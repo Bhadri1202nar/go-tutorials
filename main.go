97b71ea99249a103854310f93fece0f0c0a01feb
Bhadri Narayanan <narayananbhadri34@gmail.com>
2025-04-13 17:30:06 +0530

    Arrays and Loops

--- main.go
+++ main.go
@@ -1,53 +1,26 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
-func printMe(printValue string) {
-	fmt.Println(printValue)
-
-}
-func printNum(a int, b int) {
-	fmt.Println(a, b)
-}
-func product(a int, b int) int {
-	return a * b
-}
-func addNum(a int, b int) int {
-	return a + b
-}
-func divNum(a int, b int) (int, int, error) {
-	var err error
-	if b == 0 {
-		err = errors.New("division by zero")
-		return 0, 0, err
-
-	}
-	var result int = a / b
-	var remainder int = a % b
-	return result, remainder, err
-
-}
 func main() {
-	printValue := "Yes i have done it"
-	printMe(printValue)
-	a := 2
-	b := 0
-	printNum(a, b)
-	fmt.Println(addNum(a, b))
-	fmt.Println(product(a, b))
-
-	result, remainder, err := divNum(a, b)
-	if err != nil {
-		fmt.Println("Error:", err)
-
-	} else if remainder == 0 {
-		fmt.Println("Result:", result)
-
-	} else {
-		fmt.Println("Print the result of integer division %v with remainder %v", result, remainder)
+	intArr := [...]int{1, 2, 3, 4, 5}
+	fmt.Println("Array:", intArr)
+	intSlice := []int{4, 5, 6}
+	fmt.Println(intSlice)
+	intSlice = append(intSlice, 7)
+	fmt.Println(intSlice)
+	MyMap := make(map[string]int)
+	fmt.Println(MyMap)
+	myMap := map[string]int{"a": 1, "b": 2}
+	fmt.Println(myMap)
+	myMap1 := map[string]int{"BHadri": 21, "Barath": 25}
+	fmt.Println(myMap1["BHadri"])
+	delete(myMap1, "BHadri")
+	fmt.Println(myMap1)
+	mymap2 := map[string]int{"Bha": 21, "Bara": 25, "Mad": 51, "Ana": 54}
+	fmt.Println(mymap2)
+	for name, age := range mymap2 {
+		fmt.Printf("Name: %v, Age: %v", name, age)
 
 	}
 
