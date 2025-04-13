64e8606ecc596da83e3403a83146c2e69ba4bca8
Bhadri Narayanan <narayananbhadri34@gmail.com>
2025-04-13 14:34:36 +0530

    First Commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	var intNum int = 345
+	fmt.Println(intNum)
+	var floatNum float32 = 10.1
+	fmt.Println(floatNum)
+	var floatNum1 float64 = 2345.66
+	fmt.Println(floatNum1)
+	var intNum1 int = 12
+	var intNUm2 int = 10
+	fmt.Println(intNum1 / intNUm2)
+	fmt.Println(intNum1 % intNUm2)
+	var MyString string = `The Legend arrives`
+	fmt.Println(MyString)
+	fmt.Println(len("h"))
+}
