f1542d9104f0ab48ddef22a670c9d284649ca97e
Bhadri Narayanan <narayananbhadri34@gmail.com>
2025-04-13 16:11:01 +0530

    functions and flows

--- main.go
+++ main.go
@@ -1,20 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
+func printMe(printValue string) {
+	fmt.Println(printValue)
+
+}
+func printNum(a int, b int) {
+	fmt.Println(a, b)
+}
+func product(a int, b int) int {
+	return a * b
+}
+func addNum(a int, b int) int {
+	return a + b
+}
+func divNum(a int, b int) (int, int, error) {
+	var err error
+	if b == 0 {
+		err = errors.New("division by zero")
+		return 0, 0, err
+
+	}
+	var result int = a / b
+	var remainder int = a % b
+	return result, remainder, err
+
+}
 func main() {
-	fmt.Println("Hello, World!")
-	var intNum int = 345
-	fmt.Println(intNum)
-	var floatNum float32 = 10.1
-	fmt.Println(floatNum)
-	var floatNum1 float64 = 2345.66
-	fmt.Println(floatNum1)
-	var intNum1 int = 12
-	var intNUm2 int = 10
-	fmt.Println(intNum1 / intNUm2)
-	fmt.Println(intNum1 % intNUm2)
-	var MyString string = `The Legend arrives`
-	fmt.Println(MyString)
-	fmt.Println(len("h"))
+	printValue := "Yes i have done it"
+	printMe(printValue)
+	a := 2
+	b := 0
+	printNum(a, b)
+	fmt.Println(addNum(a, b))
+	fmt.Println(product(a, b))
+
+	result, remainder, err := divNum(a, b)
+	if err != nil {
+		fmt.Println("Error:", err)
+
+	} else if remainder == 0 {
+		fmt.Println("Result:", result)
+
+	} else {
+		fmt.Println("Print the result of integer division %v with remainder %v", result, remainder)
+
+	}
+
 }
