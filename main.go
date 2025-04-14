559244220af49f143b23a7dff7c09991e1b11b88
Bhadri Narayanan <narayananbhadri34@gmail.com>
2025-04-14 16:40:15 +0530

    structs and interfaces

--- main.go
+++ main.go
@@ -2,26 +2,45 @@ package main
 
 import "fmt"
 
-func main() {
-	intArr := [...]int{1, 2, 3, 4, 5}
-	fmt.Println("Array:", intArr)
-	intSlice := []int{4, 5, 6}
-	fmt.Println(intSlice)
-	intSlice = append(intSlice, 7)
-	fmt.Println(intSlice)
-	MyMap := make(map[string]int)
-	fmt.Println(MyMap)
-	myMap := map[string]int{"a": 1, "b": 2}
-	fmt.Println(myMap)
-	myMap1 := map[string]int{"BHadri": 21, "Barath": 25}
-	fmt.Println(myMap1["BHadri"])
-	delete(myMap1, "BHadri")
-	fmt.Println(myMap1)
-	mymap2 := map[string]int{"Bha": 21, "Bara": 25, "Mad": 51, "Ana": 54}
-	fmt.Println(mymap2)
-	for name, age := range mymap2 {
-		fmt.Printf("Name: %v, Age: %v", name, age)
+type gasEngine struct {
+	mpg     uint32
+	gallons uint32
+}
+type electricEngine struct {
+	mpkwh uint32
+	kwh   uint32
+}
 
+func (e gasEngine) milesLeft() uint32 {
+	return e.gallons * e.mpg
+}
+func (e electricEngine) milesLeft() uint32 {
+	return e.mpkwh * e.kwh
+}
+
+type engine interface {
+	milesLeft() uint32
+}
+
+func canReach(e engine, miles uint32) {
+	if miles <= e.milesLeft() {
+		fmt.Println("You can make it there!")
+
+	} else {
+		fmt.Println("Need to fuelup first")
 	}
+
+}
+
+/*type owner struct{
+	name string
+
+}*/
+func main() {
+	var myEngine gasEngine = gasEngine{mpg: 25, gallons: 15}
+	fmt.Println(myEngine.milesLeft())
+	var myE_Engine electricEngine = electricEngine{mpkwh: 15, kwh: 10}
+	fmt.Println(myE_Engine.milesLeft())
+	fmt.Println(myEngine.mpg, myEngine.gallons)
 
 }
