6acbb34988fa188405ee2fbbc4506db3bf44c4d9
Bhadri Narayanan <narayananbhadri34@gmail.com>
2025-04-15 22:05:35 +0530

    Pointers

--- main.go
+++ main.go
@@ -2,45 +2,36 @@ package main
 
 import "fmt"
 
-type gasEngine struct {
-	mpg     uint32
-	gallons uint32
-}
-type electricEngine struct {
-	mpkwh uint32
-	kwh   uint32
-}
-
-func (e gasEngine) milesLeft() uint32 {
-	return e.gallons * e.mpg
-}
-func (e electricEngine) milesLeft() uint32 {
-	return e.mpkwh * e.kwh
-}
-
-type engine interface {
-	milesLeft() uint32
-}
-
-func canReach(e engine, miles uint32) {
-	if miles <= e.milesLeft() {
-		fmt.Println("You can make it there!")
-
-	} else {
-		fmt.Println("Need to fuelup first")
-	}
-
-}
-
-/*type owner struct{
-	name string
-
-}*/
 func main() {
-	var myEngine gasEngine = gasEngine{mpg: 25, gallons: 15}
-	fmt.Println(myEngine.milesLeft())
-	var myE_Engine electricEngine = electricEngine{mpkwh: 15, kwh: 10}
-	fmt.Println(myE_Engine.milesLeft())
-	fmt.Println(myEngine.mpg, myEngine.gallons)
+	var thing1 = [5]float64{1, 2, 3, 4, 5}
+	fmt.Printf("\nThe memory location of thing1 is: %p", &thing1)
+	square(&thing1)
+
+	fmt.Printf("\nThe  updated memory location of thing1 is: %p", &thing1)
+	fmt.Printf("\nThe value of thing1 is: %v", thing1)
+
+	var p *int32 = new(int32)
+	var i int32
+	var slice = []int32{1, 2, 3}
+	var sliceCopy = slice
+	slice[2] = 4
+	fmt.Println(slice)
+	fmt.Println(sliceCopy)
+	fmt.Printf("The value p points is: %v", *p)
+	fmt.Printf("\nThe value if i is: %v", i)
+	p = &i
+	*p = 1
+	fmt.Printf("The value p points is: %v", *p)
+	fmt.Printf("\nThe value if i is: %v", i)
+
+}
+func square(thing2 *[5]float64) {
+	fmt.Printf("The value thing2 points to: %v\n", *thing2)
+	fmt.Printf("The address thing2 points to (same as &thing1): %p\n", thing2)
+	fmt.Printf("The address of thing2 variable itself: %p\n", &thing2)
+	for i := range thing2 {
+		thing2[i] *= thing2[i]
+
+	}
 
 }
